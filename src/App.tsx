8fc02fdaea4513ae00fc0c2b48fac40bb4a25474
Hassan Nadeem <hnadeem@vt.edu>
2022-07-03 08:13:33 -0400

    add TypingHeader

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import usman_nadeem from "./profile_pictures/usman_nadeem.png";
 import ar_nadeem from "./profile_pictures/ar_nadeem.png";
 import nadeem_hafeez from "./profile_pictures/nadeem_hafeez.jpg";
 import { Tree, TreeNode } from "react-organizational-chart";
+import TypeAnimation from "react-type-animation";
+
 // import FamilyTree from "./FamilyTree";
 
 const FamilyTree = () => (
@@ -57,11 +59,39 @@ const AbdulRahmanNadeem = (
 
 const MominaNadeem = <div>Momina Nadeem</div>;
 
+const names = [
+  "Hassan Nadeem",
+  "Usman Nadeem",
+  "Momina Nadeem",
+  "AbdulRahman Nadeem",
+];
+
+const TypingHeader = () => {
+  const delay = 5000;
+  const sequence = names
+    .map((name, i) => {
+      return [name, delay];
+    })
+    .flat();
+
+  return (
+    <div style={{ width: "20em" }}>
+      <TypeAnimation
+        cursor={true}
+        sequence={sequence}
+        wrapper="h2"
+        repeat={Infinity}
+      />
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h3>Welcome to Nadeem's</h3>
+        <TypingHeader />
 
         <FamilyTree />
       </header>
